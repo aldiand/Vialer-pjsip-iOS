90e055660a4574dbbf8e9780da4a92036623ee39
Andre Bahtiar Fauzi <supersandre@gmail.com>
2023-12-11 15:18:52 +0700

    feat: add to personalized_build

--- personalized_build.sh
+++ personalized_build.sh
@@ -3,3 +3,5 @@ set -e
 
 ./vialerbuild --extra-config-site-options="PJ_IPHONE_OS_HAS_MULTITASKING_SUPPORT 1,PJSIP_AUTH_HEADER_CACHING 1,PJSIP_AUTH_AUTO_SEND_NEXT 1,PJSUA_DISABLE_AUTO_SEND_100 1,PJSUA_HAS_VIDEO 0"
 ./vialerbuild-sim --extra-config-site-options="PJ_IPHONE_OS_HAS_MULTITASKING_SUPPORT 1,PJSIP_AUTH_HEADER_CACHING 1,PJSIP_AUTH_AUTO_SEND_NEXT 1,PJSUA_DISABLE_AUTO_SEND_100 1,PJSUA_HAS_VIDEO 0"
+
+xcodebuild -create-xcframework -framework iphoneos/VialerPJSIP.framework -framework iphonesimulator/VialerPJSIP.framework -output "VialerPJSIP.xcframework"
